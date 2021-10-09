513bd5ba18660e435e2b9cda6f08654e011d7d04
Arafat Islam <arafatkn@gmail.com>
2021-10-09 15:57:22 +0600

    users, quizzes, questions migrations

--- database/migrations/2021_10_09_075623_create_quizzes_table.php
+++ database/migrations/2021_10_09_075623_create_quizzes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateQuizzesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::table('quizzes', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->unsignedBigInteger('author_id')->index();
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->unsignedInteger('time_limit');
+            $table->boolean('author_digest');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
@@ -31,6 +31,8 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::table('quizzes', function (Blueprint $table) {
+            //
+        });
     }
 }
---
+++ database/migrations/2021_10_09_075946_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('quiz_id')->index();
+            $table->text('question');
+            $table->json('options');
+            $table->unsignedInteger('answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
