48a9db7214872128a62090b459dbdee2a421a48f
Arafat Islam <arafatkn@gmail.com>
2021-10-10 12:25:11 +0600

    Home controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\BSForm;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Store variables, which will be sent to view automatically
+     * @var array
+     */
+    protected $data = [];
+
+    protected $view = "public.", $route = "";
+
+    /**
+     * Store public storage object
+     */
+    protected $storage;
+
+    /**
+     * Store request object
+     */
+    protected $request;
+
+    /**
+     * current logged-in user
+     */
+    protected $user;
+
+    /**
+     * Breadcrumb links
+     */
+    protected $breadcrumbs;
+
+    public function __construct()
+    {
+        $this->storage = Storage::disk('public');
+        $this->request = request();
+    }
+
+    public function setView($view)
+    {
+        $this->view .= $view.'.';
+    }
+    public function setRoute($route)
+    {
+        $this->route .= $route.'.';
+    }
+
+    public function header()
+    {
+        if (Auth::check()) {
+            $this->user = Auth::user();
+        }
+    }
+
+    public function view($file = "index")
+    {
+        $this->data["bsform"] = new BSForm();
+        $this->data["user"] = $this->user;
+        $this->data['storage'] = $this->storage;
+        $this->data["request"] = $this->request;
+        $this->data["route"] = route($this->route.'index');
+        $this->data["breadcrumbs"] = $this->breadcrumbs;
+        return view($this->view.$file, $this->data);
+    }
+
+    public function route($file, $id = ''): string
+    {
+        if (empty($id)) {
+            return route($this->route.$file);
+        } else {
+            return route($this->route.$file, $id);
+        }
+    }
+
+    public function notfound(string $file = '')
+    {
+        $this->breadcrumbs = array('name' => 'Not Found', 'url'=> '#', );
+        if (empty($file)) {
+            return $this->view('404');
+        } else {
+            return $this->view($file);
+        }
+    }
 }
---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quiz;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    /**
+     * Home Page
+     */
+    public function index(Request $request)
+    {
+        $quizzes = Quiz::public();
+
+        if($request->has('search')) {
+            $quizzes = $quizzes->searchBy($request->search);
+        }
+
+        $this->data['quizzes'] = $quizzes->latest()->paginate();
+
+        return $this->view('index');
+    }
+}
