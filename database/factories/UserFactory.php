754f53ceb5fcae90a6ffb5969c77afe1b7bcd9cc
Arafat Islam <arafatkn@gmail.com>
2021-10-25 15:01:37 +0600

    Update User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => bcrypt('password'), // password
+            'role' => 'user',
             'remember_token' => Str::random(10),
         ];
     }
